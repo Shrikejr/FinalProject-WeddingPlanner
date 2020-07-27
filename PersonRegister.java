f3c3408ad51dd9ada192968af0b4c77dfe597e84
Shrikejr <53275118+Shrikejr@users.noreply.github.com>
2020-07-26 21:49:37 -0500

    Create PersonRegister.java

---
+++ PersonRegister.java
@@ -0,0 +1,8 @@
+public class PersonRegister {
+  public String firstName;
+  public String lastName;
+  public String userType;
+  public String password;
+  public String loginName;
+  int defaultPassLength = 9;
+}
